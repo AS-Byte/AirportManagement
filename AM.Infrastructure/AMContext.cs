a7414ce918658433b072e819cb7dccbf476c70d4
AS~byte <amine.oueslati@esprit.tn>
2023-01-24 20:24:08 +0100

    4

---
+++ AM.Infrastructure/AMContext.cs
@@ -0,0 +1,24 @@
+﻿
+using AM.ApplicationCore.Domain;
+using Microsoft.EntityFrameworkCore; //ORM: équivalent de hibernate en java
+
+namespace AM.Infrastructure
+{
+    public class AMContext:DbContext //Classe générique qui offres des méthodes permettant de configurer l'ORM
+    {
+        public DbSet <Plane> Planes { get; set; }
+        public DbSet <Flight> Flights { get; set; }
+        public DbSet <Staff> Staff { get; set; }
+        public DbSet <Passenger> Passengers { get; set; }
+
+        //chaine de connection
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=(localdb)\MSSQLLocalDB;
+                Initial Catalog=AirportManagement;
+                Integrated Security=true;MultipleActiveResultSets=true"
+                );
+        }
+    }
+}
